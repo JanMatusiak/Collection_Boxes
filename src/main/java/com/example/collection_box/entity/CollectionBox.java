c92fbaef7fba4057cb9d305753e9e0794efeeb33
JanMatusiak <jan.matusiak.2006@gmail.com>
2025-06-10 19:33:06 +0200

    Created an entity package with CollectionBox and Event classes

---
+++ src/main/java/com/example/collection_box/entity/CollectionBox.java
@@ -0,0 +1,11 @@
+package com.example.collection_box.entity;
+
+import java.math.BigDecimal;
+
+public class CollectionBox {
+    private int id;
+    private boolean empty;
+    private Event event;
+    private BigDecimal amount;
+    private String currency;
+}
---
+++ src/main/java/com/example/collection_box/entity/Event.java
@@ -0,0 +1,9 @@
+package com.example.collection_box.entity;
+
+import java.math.BigDecimal;
+
+public abstract class Event {
+    private String name;
+    private BigDecimal amount;
+    private String currency;
+}
