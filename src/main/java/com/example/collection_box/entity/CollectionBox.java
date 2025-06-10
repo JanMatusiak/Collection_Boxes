086e36a33fdfe0c8690a8c09a423d4173fdd951a
JanMatusiak <jan.matusiak.2006@gmail.com>
2025-06-10 19:56:20 +0200

    Modifications to CollectionBox, adding annotations and getters

--- src/main/java/com/example/collection_box/entity/CollectionBox.java
+++ src/main/java/com/example/collection_box/entity/CollectionBox.java
@@ -1,11 +1,44 @@
 package com.example.collection_box.entity;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CollectionBox {
-    private int id;
-    private boolean empty;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
     private Event event;
+
+    private boolean empty;
     private BigDecimal amount;
     private String currency;
+
+    protected CollectionBox(){
+        this.empty = true;
+        this.amount = BigDecimal.ZERO;
+    }
+
+    public CollectionBox(String currency){
+        this();
+        this.currency = currency;
+    }
+
+    public Event getEvent(){
+        return event;
+    }
+
+    public boolean getStatus(){
+        return empty;
+    }
+    public BigDecimal getAmount(){
+        return amount;
+    }
+    public Long getID(){
+        return id;
+    }
 }
